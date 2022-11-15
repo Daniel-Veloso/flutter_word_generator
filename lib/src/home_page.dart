dd3b8234739fd90ac8b8226b685701e7661fabd7
Daniel Veloso <danielferreiraveloso@hotmail.com>
2022-11-14 21:34:36 -0300

    Making layout changes, adding new font, images and background

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -12,18 +12,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final controller = HomeController();
 
-    _sucess(){
+  _sucess() {
     return Container(
       child: Center(
-        child: Text(controller.wordGenerator!.slip!.advice.toString()),
+        child: Text(
+          controller.wordGenerator!.slip!.advice.toString(),
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontFamily: 'Lato', fontSize: 28),
+        ),
       ),
     );
   }
 
-  _error(){
+  _error() {
     return Center(
       child: ElevatedButton(
-        onPressed: (){
+        onPressed: () {
           controller.start();
         },
         child: const Text('Try again'),
@@ -41,17 +45,18 @@ class _HomePageState extends State<HomePage> {
     return Container();
   }
 
-  stateManagement(HomeState state){
+  stateManagement(HomeState state) {
     switch (state) {
-      case HomeState.start :
+      case HomeState.start:
         return _start();
-      case HomeState.loading :
+      case HomeState.loading:
         return _loading();
-      case HomeState.success :
+      case HomeState.success:
         return _sucess();
-      case HomeState.error :
+      case HomeState.error:
         return _error();
-      default: return _start();
+      default:
+        return _start();
     }
   }
 
@@ -64,22 +69,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Word Generator'),
-        actions: [
-          IconButton(
-            onPressed: (){
-              controller.start();
-            }, 
-            icon: const Icon(Icons.refresh_outlined),
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Container(height: 70),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: AnimatedBuilder(
+                  animation: controller.state,
+                  builder: (context, child) {
+                    return stateManagement(controller.state.value);
+                  },
+                ),
+              ),
+              Container(height: 80),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: SizedBox(
+                  height: 50,
+                  width: 180,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      controller.start();
+                    }, 
+                    child: const Text(
+                      'New sentence',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
-      ),
-      body: AnimatedBuilder(
-        animation: controller.state,
-        builder: (context, child) {
-          return stateManagement(controller.state.value);
-        },
       ),
     );
   }
